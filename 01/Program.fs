5fdf28831ee052300b9483e84be6e11d6f6e400f
Erik Göök <erikgook@gmail.com>
2019-12-01 20:58:42 +0100

    First

---
+++ 01/Program.fs
@@ -0,0 +1,14 @@
+﻿open System.IO
+
+let flip f a b = f b a
+
+let fuelCost = (flip (-) 2) << (flip (/) 3)
+
+[<EntryPoint>]
+let main argv =
+    let r = File.ReadAllLines(argv.[0]) 
+            |> Seq.map int
+            |> Seq.fold (fun acc v -> acc + (fuelCost v)) 0
+
+    printfn "%i" r
+    0 
