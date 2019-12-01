9c2f8aa6fc33f3b34b80e249b93bb4c5bdab377f
Erik Göök <erikgook@gmail.com>
2019-12-01 21:31:36 +0100

    01 second question

--- 01/Program.fs
+++ 01/Program.fs
@@ -1,14 +1,22 @@
 ﻿open System.IO
+open Microsoft.FSharp.Core.Operators.Checked
 
 let flip f a b = f b a
 
 let fuelCost = (flip (-) 2) << (flip (/) 3)
 
+let rec compFuelCost (x) =
+       match (x < 9) with
+        | true -> 0
+        | false -> let c = fuelCost x in c + compFuelCost c
+        
+         
+
 [<EntryPoint>]
 let main argv =
     let r = File.ReadAllLines(argv.[0]) 
             |> Seq.map int
-            |> Seq.fold (fun acc v -> acc + (fuelCost v)) 0
+            |> Seq.fold (fun acc v -> acc + (compFuelCost v)) 0
 
     printfn "%i" r
     0 
