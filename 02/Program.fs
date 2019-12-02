2a8bb9133b774674c682855879d77699bb025054
Erik Göök <erikgook@gmail.com>
2019-12-02 18:29:09 +0100

    02 a

---
+++ 02/Program.fs
@@ -0,0 +1,50 @@
+﻿open System.IO
+open Microsoft.FSharp.Core.Operators.Checked
+
+let opCode1 (p:int) (xs:int[]) =
+    let (lhs, rhs, reg) = (xs.[p + 1], xs.[p + 2], xs.[p + 3])
+    // printfn "p: %i lhs: %i rhs %i reg %i" p lhs rhs reg
+
+    Array.set xs reg (xs.[lhs] + xs.[rhs])
+
+    xs
+
+ 
+
+let opCode2 (p:int) (xs:int[]) =
+    let (lhs, rhs, reg) = (xs.[p + 1], xs.[p + 2], xs.[p + 3])
+
+    Array.set xs reg (xs.[lhs] * xs.[rhs])
+
+    xs
+
+ 
+
+let rec compute (p:int) (xs:int[]) =
+
+    match xs.[p] with
+        | 1  -> let xs2 = opCode1 p xs in compute (p + 4) xs2
+        | 2  -> let xs2 = opCode2 p xs in compute (p + 4) xs2
+        | 99 -> xs
+        | _  -> [||]
+
+    
+
+ 
+
+[<EntryPoint>]
+
+let main argv =
+
+    let initial = (File.ReadAllText argv.[0]).Split([|','|])
+                   |> Array.map int
+
+    Array.set initial 1 12
+    Array.set initial 2  2
+
+    let r = compute 0 initial
+    // printfn "%s" <| String.Join (", ", Array.map string r)    
+
+    printfn "%i" r.[0]
+
+    0 // return an integer exit code
