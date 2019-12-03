139d88aaf5cf54a6ebd85223751824f92046819d
Erik Göök <erik.goek@cab.se>
2019-12-03 15:23:52 +0100

    wierdness from remote

--- 03/Program.fs
+++ 03/Program.fs
@@ -32,25 +32,6 @@ let run dirs = Seq.fold follow (Seq.singleton (0,0))  dirs
 
 let manhattanD (a, b) = abs a + abs b
 
-type Direction = Up | Down | Left | Right
-
-type Instruction = Direction * int
-
-let parseDirection (s:string) = 
-    match (s.Substring(0, 1), int <| s.Substring(1, s.Length - 1)) with
-        | ("U", d) -> (Up, d)
-        | ("D", d) -> (Down, d)
-        | ("L", d) -> (Left, d)
-        | ("R", d) -> (Right, d)
-        | _ -> (Up, 0)
-        
-let path (x, y) (dir, dis) =
-    match dir with
-        | Up    -> Seq.init dis (fun n -> y + n + 1) |> Seq.map (fun v -> (x, v))
-        | Down  -> Seq.init dis (fun n -> y - n - 1) |> Seq.map (fun v -> (x, v))
-        | Left  -> Seq.init dis (fun n -> x - n - 1) |> Seq.map (fun v -> (v, y))
-        | Right -> Seq.init dis (fun n -> x + n + 1) |> Seq.map (fun v -> (v, y))
-
 [<EntryPoint>]
 let main argv =
     let dirs = (File.ReadAllText <| argv.[0]).Split ","
