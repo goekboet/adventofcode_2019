a1ad0bf1a8465e2e26ba698f3f53d720a5757dc3
Erik Göök <erik.goek@cab.se>
2019-12-10 10:03:47 +0100

    10 wip

---
+++ 10/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+
+type Map = string[]
+type Position = int * int
+type AsteroidPositions = Set<Position>
+
+let pair x y = (x, y)
+let place ((x : int), (ys : seq<char>)) = 
+    Seq.mapi (fun i y -> ((x, i), y = '#' )) ys
+    |> Seq.filter snd
+    |> Seq.map fst
+
+let readPos (map : Map) =
+    Seq.mapi pair map
+    |> Seq.collect place
+    |> Set.ofSeq
+
+[<EntryPoint>]
+let main argv =
+    let map = File.ReadAllLines("test.txt")
+    let r = readPos map
+    printfn "Asteroids: %A" r
+    0 // return an integer exit code
