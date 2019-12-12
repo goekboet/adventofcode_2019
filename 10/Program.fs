7a4de23ee2168fc7bd7db35450c94924a3f7c7ae
Erik Göök <erikgook@gmail.com>
2019-12-12 13:00:42 +0100

    10 pt 2

--- 10/Program.fs
+++ 10/Program.fs
@@ -56,39 +56,87 @@ let mostLineOfSights (map : Set<Position>) =
         |> List.map (fun (k, ps) -> (k, (List.map (fun p -> firstCollision map p) ps |> List.distinct).Length ))
         |> List.maxBy snd    
 
-type Direction = U | R | D | L
-let clockwise = [U;R;D;L]
+type Polar = float * float
 
-let steps = Seq.initInfinite id |> Seq.collect (fun x -> [x + 1;x + 1])
-let dirs  = Seq.initInfinite id |> Seq.collect (fun _ -> clockwise)
-let cwSpiral = Seq.zip dirs steps
+let toPolar (x, y) =
+    let r = sqrt (float (x * x + y * y))
+    let t = atan2 (float y) (float x)
 
+    if t < 0.0
+    then (r, t + (2.0 * Math.PI))
+    else (r, t)
 
-let move (x, y) (dir, offset) =
-    match dir with
-        | U -> (x, y + offset)
-        | R -> (x + offset, y)
-        | D -> (x, y - offset)
-        | L -> (x - offset, y)
+let ccw90 (r, t) = 
+    let (r, t') = (r, t - Math.PI * (1.0 / 2.0))
+    if t' <= 0.0
+    then (r, t' + Math.PI * 2.0)
+    else (r, t')
 
-let cwSpiralFrom x =
-    Seq.scan move x cwSpiral |> Seq.pairwise |> Seq.collect (fun (a, b) -> inLine a b |> Seq.skip 1)
+let parsemap (map : Map) =
+    let w = map.[0].Length
+    
+    let o = Seq.collect id map
+                 |> Seq.indexed
+                 |> Seq.map (fun (i, x) -> ((i % w, i / w), x))
+                 |> Seq.find ((=) 'X' << snd)
+                 |> fst
 
-let cwSpiralOrder origin map =
-    cwSpiralFrom origin 
-    |> Seq.scan (fun (uo, o) x -> if Set.contains x uo then (Set.remove x uo, x :: o) else (uo, o)) (map, [])
-    |> Seq.skipWhile (fun (uo, o) -> not <| Set.isEmpty uo)
-    |> Seq.head
-    |> snd
-    |> Seq.rev
+    let mapping (x, y) w (i, _) =
+        let (x', y') = (i % w, i / w)
+        let polar = toPolar (x' - x, y - y') |> ccw90
+        
+        (polar, (x', y'))
+
+    Seq.collect id map
+    |> Seq.indexed
+    |> Seq.filter ((=) '#' << snd)
+    |> Seq.map (mapping o w)
 
 [<EntryPoint>]
 let main argv =
-    let map = File.ReadAllLines(argv.[0])
-                |> readPos
+    let data = File.ReadAllLines(argv.[0])
+
+    // let best = 
+    //     data
+    //     |> readPos
+    //     |> mostLineOfSights
     
-    let (o, _) = mostLineOfSights map
+    let xs = 
+        data
+        |> parsemap
     
-    
-    //printfn "Monitor: %s" <| string r
+    let mapping xs = 
+        Seq.sortBy (fun ((r, t), _) -> r) xs
+        |> Seq.map (fun (_, x) -> x)
+        |> Seq.toList
+
+    let xs' = 
+        xs 
+        |> Seq.groupBy (fun ((_, t), _) -> t)
+        |> Seq.sortByDescending fst
+        |> Seq.map (fun (_, xs) -> mapping xs)
+        |> Seq.toArray
+
+    let order ((xs : list<Position>[]), rs) (i : int) =
+        match xs.[i] with
+            | x :: r  -> 
+                Array.set xs i r
+                (xs , x :: rs)
+            | []      ->
+                (xs, rs)
+
+    let (x, y) =
+        Seq.initInfinite (fun i -> i % xs'.Length)
+        |> Seq.scan order (xs', [])
+        |> Seq.skip (Seq.length xs - 1)
+        |> Seq.map snd
+        |> Seq.head
+        |> Seq.rev
+        |> Seq.skip 199
+        |> Seq.head
+
+    printfn "%i" (x * 100 + y)
+
+    // Seq.iteri (fun i x -> printfn "%4i %A" i x) r
+              
     0 // return an integer exit code
